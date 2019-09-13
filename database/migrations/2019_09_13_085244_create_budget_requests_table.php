44456c66b4927967512637f3e7b38aa97c823bb5
AlejandroSuau <alejandro.suau@gmail.com>
2019-09-13 12:12:03 +0200

    Fixed an error in name class

--- database/migrations/2019_09_13_085244_create_budget_requests_table.php
+++ database/migrations/2019_09_13_085244_create_budget_requests_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBudgetRequestTable extends Migration
+class CreateBudgetRequestsTable extends Migration
 {
     /**
      * Run the migrations.
