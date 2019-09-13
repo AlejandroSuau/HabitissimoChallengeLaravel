1cb04cdc74a6889efa0818d9e02bf5d4e7d5870b
AlejandroSuau <alejandro.suau@gmail.com>
2019-09-13 12:49:54 +0200

    Fixed a version error

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(BudgetRequestStatusSeeder::class);
+        $this->call(BudgetRequestCategoriesSeeder::class);
     }
 }
