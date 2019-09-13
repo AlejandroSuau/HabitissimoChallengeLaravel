6525e22d1e17d2939c2ecc0533aa430b0d0c12a6
AlejandroSuau <alejandro.suau@gmail.com>
2019-09-13 12:10:26 +0200

    Created DB structured which is currently necessary

---
+++ database/migrations/2019_09_13_085144_create_budget_request_status_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBudgetRequestStatusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('budget_request_status', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('budget_request_status');
+    }
+}
---
+++ database/migrations/2019_09_13_085244_create_budget_requests_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBudgetRequestTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('budget_requests', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title')->nullable();
+            $table->string('description');
+
+            $table->integer('category_id')->unsigned()->nullable();
+            $table->foreign('category_id')->references('id')->on('budget_categories')
+                ->onDelete('cascade'); // 'On Cascade just to allow us to clean DB for every test'
+
+            $table->integer('budget_request_status_id')->unsigned();
+            $table->foreign('budget_request_status_id')->references('id')->on('budget_request_status')
+                ->onDelete('cascade'); // 'On Cascade just to allow us to clean DB for every test'
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('budget_request');
+    }
+}
